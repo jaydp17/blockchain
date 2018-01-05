b459df1982fdc6b48897d5a03c0bfb3f33ed2d8c
Jaydeep Solanki <jaydp17@gmail.com>
2018-01-05 17:31:36 +0530

    implement /mine & /transactions/new methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const restify = require('restify');
 const uuid4 = require('uuid4');
+const errs = require('restify-errors');
 const BlockChain = require('./blockchain');
 
 const blockChain = new BlockChain();
@@ -10,13 +11,41 @@ const server = restify.createServer({
   version: '1.0.0',
 });
 
+server.use(restify.plugins.bodyParser());
+
 server.get('/mine', (req, res, next) => {
-  res.send({ message: "we'll mine a new Block" });
+  const lastBlock = blockChain.lastBlock;
+  const lastProof = lastBlock.proof;
+  const currentProof = BlockChain.calculateProof(lastProof);
+
+  // The Miner must receive a reward for finding the proof.
+  // The sender is '0' to signify that this node has mined a new coin.
+  blockChain.addTransaction({
+    sender: '0',
+    recipient: nodeName,
+    amount: 1,
+  });
+
+  const previousHash = BlockChain.hash(blockChain.lastBlock);
+  const newBlock = blockChain.addBlock({ proof: currentProof, previousHash });
+
+  res.send({
+    ...newBlock,
+    message: 'New block forged',
+  });
   next();
 });
 
 server.post('/transactions/new', (req, res, next) => {
-  res.send({ message: "we'll adda new transaction" });
+  const { sender, recipient, amount } = req.body;
+  if (!sender || !recipient || !amount) {
+    return next(new errs.BadRequestError('Missing parameters'));
+  }
+
+  // create a new Transaction
+  const blockIndex = blockChain.addTransaction({ sender, recipient, amount });
+
+  res.send({ message: `Transaction will be added to Block ${blockIndex}` });
   next();
 });
 
